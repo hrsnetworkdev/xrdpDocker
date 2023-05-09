2755f935ce39c4f49024d1a2e3a6e22b9d886e73
hrsnetworkdev <130751804+hrsnetworkdev@users.noreply.github.com>
2023-05-10 00:21:43 +0700

    Update xrdp-ngrok.sh

--- xrdp-ngrok.sh
+++ xrdp-ngrok.sh
@@ -11,7 +11,7 @@ sleep 10
 NGROK_URL=$(curl -s localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
 
 # Menambahkan alamat URL ngrok ke file xrdp
-echo "address=${NGROK_URL#tcp://}" | tee /etc/xrdp/xrdp.ini > /dev/null
+echo "address=${NGROK_URL#tcp://}" | sudo tee /etc/xrdp/xrdp.ini > /dev/null
 
 # Memulai ulang xrdp
 service xrdp restart
